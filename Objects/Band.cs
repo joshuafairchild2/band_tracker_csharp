8a283b2b71fc423778f8e034090e480cf9679412
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-16 09:18:26 -0700

    pass test from previous commmit

--- Objects/Band.cs
+++ Objects/Band.cs
@@ -19,7 +19,31 @@ namespace BandTracker.Objects
 
     public static List<Band> GetAll()
     {
-      return null;
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands", conn);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Band> allBands = new List<Band>{};
+      while (rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        int numbOfMembs = rdr.GetInt32(2);
+
+        Band newBand = new Band(name, numbOfMembs, id);
+        allBands.Add(newBand);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+
+      return allBands;
     }
   }
 }
--- Objects/Database.cs
+++ Objects/Database.cs
@@ -5,10 +5,18 @@ namespace BandTracker.Objects
 {
   public class DB
   {
+    private static SqlConnection _conn;
     public static SqlConnection Connection()
     {
       SqlConnection conn = new SqlConnection(DBConfiguration.ConnectionString);
       return conn;
+    }
+    public static void CloseConnection()
+    {
+      if(_conn != null)
+      {
+        _conn.Close();
+      }
     }
   }
 }
