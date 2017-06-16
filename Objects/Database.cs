a5ee3863059c7a99b4b4478189441f25b6459518
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-16 16:55:42 -0700

    remove something that is no longer being used

--- Objects/Database.cs
+++ Objects/Database.cs
@@ -5,18 +5,10 @@ namespace BandTracker.Objects
 {
   public class DB
   {
-    private static SqlConnection _conn;
     public static SqlConnection Connection()
     {
       SqlConnection conn = new SqlConnection(DBConfiguration.ConnectionString);
       return conn;
-    }
-    public static void CloseConnection()
-    {
-      if(_conn != null)
-      {
-        _conn.Close();
-      }
     }
   }
 }
