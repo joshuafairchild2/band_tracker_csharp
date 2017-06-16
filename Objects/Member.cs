dd5522214bf441c587f88e02c705b9e4e7902676
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-06-16 10:29:49 -0700

    all tests now passing

--- Objects/Member.cs
+++ Objects/Member.cs
@@ -10,6 +10,13 @@ namespace BandTracker.Objects
     public string Name {get;set;}
     public int BandId {get;set;}
 
+    public Member()
+    {
+      Id = 0;
+      Name = null;
+      BandId = 0;
+    }
+
     public Member(string name, int bandId, int id = 0)
     {
       Id = id;
@@ -17,24 +24,108 @@ namespace BandTracker.Objects
       BandId = bandId;
     }
 
+    public override bool Equals(System.Object otherMember)
+    {
+      if (!(otherMember is Member))
+      {
+        return false;
+      }
+      else
+      {
+        Member newMember = (Member) otherMember;
+        return (this.Id == newMember.Id &&
+                this.Name == newMember.Name &&
+                this.BandId == newMember.BandId);
+      }
+    }
+
     public static List<Member> GetAll()
     {
-      return null;
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM members", conn);
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      List<Member> allMembers = new List<Member>{};
+      while (rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        int bandId = rdr.GetInt32(2);
+
+        Member newMember = new Member(name, bandId, id);
+        allMembers.Add(newMember);
+      }
+
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+
+      return allMembers;
     }
 
     public void Save()
     {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
 
+      SqlCommand cmd = new SqlCommand("INSERT INTO members (name, band_id) OUTPUT INSERTED.id VALUES (@MemberName, @BandId)", conn);
+
+      cmd.Parameters.Add(new SqlParameter("@MemberName", this.Name));
+      cmd.Parameters.Add(new SqlParameter("@BandId", this.BandId));
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+      while(rdr.Read())
+      {
+        this.Id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
     }
 
     public static Member Find(int idToFind)
     {
-      return null;
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM members WHERE id = @MemberId", conn);
+      cmd.Parameters.Add(new SqlParameter("@MemberId", idToFind));
+
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      Member foundMember = new Member();
+      while (rdr.Read())
+      {
+        foundMember.Id = rdr.GetInt32(0);
+        foundMember.Name = rdr.GetString(1);
+        foundMember.BandId = rdr.GetInt32(2);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      DB.CloseConnection();
+
+      return foundMember;
     }
 
     public static void DeleteAll()
     {
-      
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("DELETE FROM members", conn);
+      cmd.ExecuteNonQuery();
+      DB.CloseConnection();
     }
   }
 }
